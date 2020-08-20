36ec63e009caa3ddf703a4f93032df6452382382
Ivan Ivanov <runner213@gmail.com>
2020-08-20 15:04:53 +0300

    Initial Commit

---
+++ Ivanov HW 2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ivanov HW 2.1
+//
+//  Created by Иван Иванов on 20.08.2020.
+//  Copyright © 2020 Ivan Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
